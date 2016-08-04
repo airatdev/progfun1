b4c3ecc65d77e07d0be433fb879c69845e61e284
Airat Shigapov <contact@airatshigapov.com>
2016-08-04 17:52:47 +0300

    Add example homework

---
+++ example/src/main/scala/example/Main.scala
@@ -0,0 +1,9 @@
+package example
+
+/**
+  * Created by airatshigapov on 04/08/16.
+  */
+object Main extends App {
+  println(Lists.max(List(1,3,2,12,0,-10)))
+  println(Lists.sum(List(1,3,2,12,0,-10)))
+}
